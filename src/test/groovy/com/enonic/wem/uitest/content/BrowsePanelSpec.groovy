f7cc03fbdacb1942d377e6bd8b1bdff0d3226603
Sergei Gavrusev <sig@enonic.com>
2014-02-06 18:30:30 +0300

    minor change

--- src/test/groovy/com/enonic/wem/uitest/content/BrowsePanelSpec.groovy
+++ src/test/groovy/com/enonic/wem/uitest/content/BrowsePanelSpec.groovy
@@ -18,9 +18,7 @@ class BrowsePanelSpec extends BaseGebSpec
 
 	def "Given BrowsePanel When adding Folder to root  Then the content should be listed in the table"() 
 	{		
-		if (!browser.baseUrl) {
-			browser.baseUrl = "http://localhost:8080/"
-	}
+		
 		given:
 		go "admin"		
 	
