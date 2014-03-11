5efde020d11a6c49429b2fab4cd4de4cba1ad19c
Sergei Gavrusev <sig@enonic.com>
2014-03-11 15:06:27 +0300

    screenshot saving added

--- src/test/groovy/com/enonic/wem/uitest/content/ContentBrowsePanel_FilterPanel_Spec.groovy
+++ src/test/groovy/com/enonic/wem/uitest/content/ContentBrowsePanel_FilterPanel_Spec.groovy
@@ -1,8 +1,14 @@
 package com.enonic.wem.uitest.content
 
 import com.enonic.autotests.exceptions.TestFrameworkException
+import com.enonic.autotests.pages.contentmanager.browsepanel.ContentBrowseFilterPanel;
 import com.enonic.autotests.pages.contentmanager.browsepanel.ContentBrowsePanel
 import com.enonic.autotests.services.NavigatorHelper
+import com.enonic.autotests.utils.NameHelper;
+import com.enonic.autotests.utils.TestUtils;
+import com.enonic.autotests.vo.contentmanager.ArchiveContent;
+import com.enonic.autotests.vo.contentmanager.BaseAbstractContent;
+import com.enonic.wem.api.content.ContentPath;
 import com.enonic.wem.uitest.BaseGebSpec
 import spock.lang.Shared
 
@@ -12,6 +18,8 @@ class ContentBrowsePanel_FilterPanel_Spec
 
     @Shared
     ContentBrowsePanel contentBrowsePanel
+	@Shared
+	ContentBrowseFilterPanel filterPanel
 
     @Shared
     String PAGE_CONTENT_TYPE_NAME = "Page"
@@ -20,13 +28,15 @@ class ContentBrowsePanel_FilterPanel_Spec
     def setup()
     {
         go "admin"
-        contentBrowsePanel = NavigatorHelper.openContentApp( getTestSession() );
+        contentBrowsePanel = NavigatorHelper.openContentApp( getTestSession() )
+		filterPanel = contentBrowsePanel.getFilterPanel()
     }
 
     def "GIVEN No selections in filter WHEN Selecting one entry in any filter THEN Clean Filter link should appear"()
     {
         when:
-        String label = contentBrowsePanel.getFilterPanel().selectEntryInContentTypesFilter( PAGE_CONTENT_TYPE_NAME )
+        String label = filterPanel.selectEntryInContentTypesFilter( PAGE_CONTENT_TYPE_NAME )
+		TestUtils.saveScreenshot(getTestSession())
 
         then:
         contentBrowsePanel.getContentNamesFromBrowsePanel().size() == parseFilterLabel( label )
@@ -35,27 +45,32 @@ class ContentBrowsePanel_FilterPanel_Spec
     def "GIVEN Selections in any filter WHEN clicking CleanFilter THEN CleanFilter link should dissapear"()
     {
         given:
-        String label = contentBrowsePanel.getFilterPanel().selectEntryInContentTypesFilter( PAGE_CONTENT_TYPE_NAME )
-
+        String label = filterPanel.selectEntryInContentTypesFilter( PAGE_CONTENT_TYPE_NAME )
+		
         when:
-        contentBrowsePanel.getFilterPanel().clickByCleanFilter()
-
+        filterPanel.clickByCleanFilter()
+		TestUtils.saveScreenshot(getTestSession())
+		
         then:
-        contentBrowsePanel.getFilterPanel().waitForClearFilterLinkNotvisible();
+		filterPanel.waitForClearFilterLinkNotvisible();
     }
-
-
-    def "GIVEN Selections in any filter WHEN clicking CleanFilter THEN all selections should dissapear"()
-    {
-        given:
-        String label = contentBrowsePanel.getFilterPanel().selectEntryInContentTypesFilter( PAGE_CONTENT_TYPE_NAME )
-
-        when:
-        contentBrowsePanel.getFilterPanel().clickByCleanFilter()
-
-        then:
-        !contentBrowsePanel.getFilterPanel().isAnySelectionPresent()
-    }
+	
+	
+	def "GIVEN Selections in any filter WHEN clicking CleanFilter THEN all selections should dissapear"()
+	{
+		given:
+		String label = filterPanel.selectEntryInContentTypesFilter( PAGE_CONTENT_TYPE_NAME )
+		
+		when:
+		contentBrowsePanel.getFilterPanel().clickByCleanFilter()
+		TestUtils.saveScreenshot(getTestSession())
+		
+		then:
+		!contentBrowsePanel.getFilterPanel().isAnySelectionPresent()
+	}
+	
+	
+	
 
     int parseFilterLabel( String label )
     {
