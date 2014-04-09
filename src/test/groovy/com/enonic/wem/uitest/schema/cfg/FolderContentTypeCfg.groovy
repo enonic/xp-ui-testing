c09a55d42ad4b1939734fbd1596af19de2703f4e
Sergei Gavrusev <sig@enonic.com>
2014-04-09 18:35:53 +0300

    minor change

--- src/test/groovy/com/enonic/wem/uitest/schema/cfg/FolderContentTypeCfg.groovy
+++ src/test/groovy/com/enonic/wem/uitest/schema/cfg/FolderContentTypeCfg.groovy
@@ -3,16 +3,15 @@ package com.enonic.wem.uitest.schema.cfg
 class FolderContentTypeCfg
 {
     static def FOLDER_CFG = '''
-	<type>
-	  <module>system</module>
-      <display-name>ctFolder</display-name>
-      <content-display-name-script />
-	  <super-type>folder</super-type>
-	  <is-abstract>false</is-abstract>
-	  <is-built-in>true</is-built-in>
-	  <is-final>false</is-final>
-	  <allow-children>true</allow-children>
-	  <form />
-    </type>
-	'''
+<type>
+	<module>system</module>
+    <display-name>ctFolder</display-name>
+    <content-display-name-script />
+	<super-type>folder</super-type>
+	<is-abstract>false</is-abstract>
+	<is-built-in>true</is-built-in>
+	<is-final>false</is-final>
+	<allow-children>true</allow-children>
+	<form />
+</type>	'''
 }
