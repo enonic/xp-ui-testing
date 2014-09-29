ec350c233c5395c49584e7fa898d1315d9b63701
Sergei Gavrusev <sig@enonic.com>
2014-09-29 12:31:06 +0300

    screenshot saving added

--- src/test/groovy/com/enonic/wem/uitest/content/ResetAppSpec.groovy
+++ src/test/groovy/com/enonic/wem/uitest/content/ResetAppSpec.groovy
@@ -2,6 +2,7 @@ package com.enonic.wem.uitest.content
 
 import com.enonic.autotests.pages.versiontest.VersionTestPage
 import com.enonic.autotests.utils.SleepHelper
+import com.enonic.autotests.utils.TestUtils
 import com.enonic.wem.uitest.BaseGebSpec
 import spock.lang.Shared
 
@@ -22,6 +23,7 @@ class ResetAppSpec
     def "clickOnReset"()
     {
         when:
+        TestUtils.saveScreenshot( getTestSession(), "versiontest2-opened" );
         versiontestPage.clickResetWem();
         versiontestPage.waitUntilResetWemButtonEnabled();
         SleepHelper.sleep( 100000 );
