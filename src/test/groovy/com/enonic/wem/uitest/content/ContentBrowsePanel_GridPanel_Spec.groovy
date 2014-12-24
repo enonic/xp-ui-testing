131ed0c275cbb31ba63e6932d915e5a0cd689eb1
Sergei Gavrusev <sig@enonic.com>
2014-12-24 16:13:22 +0300

    fix failed for test

--- src/test/groovy/com/enonic/wem/uitest/content/ContentBrowsePanel_GridPanel_Spec.groovy
+++ src/test/groovy/com/enonic/wem/uitest/content/ContentBrowsePanel_GridPanel_Spec.groovy
@@ -22,6 +22,9 @@ class ContentBrowsePanel_GridPanel_Spec
     @Shared
     String FOLDER_WITH_CHILD = NameHelper.uniqueName( "folder-test" );
 
+    @Shared
+    String FOLDER_TEST_NAME = NameHelper.uniqueName( "folder-test" );
+
 
     def setup()
     {
