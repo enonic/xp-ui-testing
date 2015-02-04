61a50fb7bb7b64f9f52afab88c46269dd0705187
Siarhei Gauruseu <sig@enonic.com>
2015-02-04 14:32:51 +0100

    saving  of screenshot added

--- selenium-tests/src/main/java/com/enonic/autotests/pages/HomePage.java
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/HomePage.java
@@ -9,6 +9,7 @@ import com.enonic.autotests.pages.contentmanager.browsepanel.ContentBrowsePanel;
 import com.enonic.autotests.pages.modules.ModuleBrowsePanel;
 import com.enonic.autotests.pages.usermanager.browsepanel.UserBrowsePanel;
 import com.enonic.autotests.services.NavigatorHelper;
+import com.enonic.autotests.utils.TestUtils;
 
 /**
  * Page Object for 'Home' page. Version 5.0
@@ -80,6 +81,7 @@ public class HomePage
 
     public ContentBrowsePanel openContentManagerApplication()
     {
+        TestUtils.saveScreenshot( getSession(), "cm" );
         contentManager.click();
         NavigatorHelper.switchToIframe( getSession(), Application.CONTENT_MANAGER_FRAME_XPATH );
         ContentBrowsePanel panel = new ContentBrowsePanel( getSession() );
