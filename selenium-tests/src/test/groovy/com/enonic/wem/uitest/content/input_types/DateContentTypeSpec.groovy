b5b426ae4f41f6f552e346a31c6ada0bde0686f4
Sergei Gavrusev <sig@enonic.com>
2015-02-20 11:15:42 +0300

    fix for one failed test

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/input_types/DateContentTypeSpec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/input_types/DateContentTypeSpec.groovy
@@ -1,4 +1,22 @@
 package com.enonic.wem.uitest.content.input_types
 
+import com.enonic.autotests.pages.contentmanager.browsepanel.ContentBrowsePanel
+import com.enonic.autotests.services.NavigatorHelper
+import com.enonic.wem.uitest.BaseGebSpec
+import spock.lang.Shared
+
+
 class DateContentTypeSpec
-{}
+    extends BaseGebSpec
+
+{
+
+    @Shared
+    ContentBrowsePanel contentBrowsePanel;
+
+    def setup()
+    {
+        go "admin"
+        contentBrowsePanel = NavigatorHelper.openContentApp( getTestSession() );
+    }
+}
