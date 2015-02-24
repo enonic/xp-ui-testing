7a0dbd4a25cc373cda8cd68a3f2ae86ab8dc66e4
Sergei Gavrusev <sig@enonic.com>
2015-02-24 14:53:02 +0300

    XP-24 Write tests for login - implemented  tests for login button

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/LoginSpec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/LoginSpec.groovy
@@ -1,35 +1,28 @@
 package com.enonic.wem.uitest
 
+import com.enonic.autotests.utils.TestUtils
 import spock.lang.Ignore
 
 class LoginSpec
     extends BaseGebSpec
 {
-    @Ignore
+    def setup()
+    {
+        go "admin"
+        getTestSession();
+
+    }
+
     def "Given login page When both username and password fields is empty Then Login Button must be disabled"()
     {
         when:
         go "admin"
 
         then:
-        $( 'button.login-button' ).classes().contains( 'disabled' );
+        $( 'button.login-button' ).getAttribute( "style" ).contains( 'display: none' );
     }
 
-    @Ignore
-    def "Given login page When both username and password fields have value Then Login Button must be enabled"()
-    {
-        given:
-        go "admin"
 
-        when:
-        $( 'input.form-item', 0 ) << 'user';
-        $( 'input.form-item', 1 ) << 'password';
-
-        then:
-        !$( 'button.login-button' ).classes().contains( 'disabled' )
-    }
-
-    @Ignore
     def "Given login page When only username field have value Then Login Button must be disabled"()
     {
         given:
@@ -38,11 +31,12 @@ class LoginSpec
         when:
         $( 'input.form-item', 0 ) << 'user';
         report "login page, username is 'user', password is empty";
+        TestUtils.saveScreenshot( getSession(), "pass_empty" );
         then:
-        $( 'button.login-button' ).classes().contains( 'disabled' );
+        $( 'button.login-button' ).getAttribute( "style" ).contains( 'display: none' );
     }
 
-    @Ignore
+
     def "Given login page When only password field have value Then Login Button must be disabled"()
     {
         given:
@@ -51,8 +45,35 @@ class LoginSpec
         when:
         $( 'input.form-item', 1 ) << 'password';
         report "login page, username is empty, password is 'password'";
+        TestUtils.saveScreenshot( getSession(), "login_empty" )
+
         then:
-        $( 'button.login-button' ).classes().contains( 'disabled' );
+        $( 'button.login-button' ).getAttribute( "style" ).contains( 'display: none' );
     }
+
+    def "Given login page When both username and password fields have value Then Login Button must be enabled"()
+    {
+
+        when:
+        $( 'input.form-item', 0 ) << 'admin';
+        $( 'input.form-item', 1 ) << 'password';
+        TestUtils.saveScreenshot( getSession(), "login_pass" )
+
+        then:
+        !$( 'button.login-button' ).getAttribute( "style" ).contains( 'display: none' );
+    }
+
+    @Ignore
+    def "Given login page When wrong username or password typed Then error message appears"()
+    {
+        when:
+        $( 'input.form-item', 0 ) << 'admin';
+        $( 'input.form-item', 1 ) << 'password';
+
+        then:
+        waitFor { $( 'div.message-container' ).text() == 'Login failed!' }
+
+    }
+
 
 }
