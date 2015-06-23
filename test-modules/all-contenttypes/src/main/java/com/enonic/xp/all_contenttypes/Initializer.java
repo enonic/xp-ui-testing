c8293c0b961fede3fa229cb08b8a248641a333c5
Sergei Gavrusev <sig@enonic.com>
2015-06-23 11:04:37 +0300

    gradle task updated

--- test-modules/all-contenttypes/src/main/java/com/enonic/xp/all_contenttypes/Initializer.java
+++ test-modules/all-contenttypes/src/main/java/com/enonic/xp/all_contenttypes/Initializer.java
@@ -65,7 +65,7 @@ public class Initializer
         throws Exception
     {
         final ContentPath imagesPath = ContentPath.from( "/" + FOLDER_NAME );
-        if ( hasContent( imagesPath ) )
+        if ( !hasContent( imagesPath ) )
         {
             return;
         }
@@ -149,7 +149,6 @@ public class Initializer
     private void createImageContent( final ContentPath parent, final String fileName )
         throws Exception
     {
-
         final byte[] bytes = loadImageFileAsBytes( fileName );
         if ( bytes == null )
         {
@@ -185,128 +184,3 @@ public class Initializer
         return ContextBuilder.from( ContextAccessor.current() ).authInfo( authInfo ).build().callWith( runnable );
     }
 }
-
-//    private final static Logger LOG = LoggerFactory.getLogger( Initializer.class );
-//
-//    private static final String[] FOLDER_IMAGES =
-//        {"book.jpg", "man.jpg", "man2.jpg", "fl.jpg", "nord.jpg", "whale.jpg", "elephant.png", "dollar.png"};
-//
-//
-//    private static final String FOLDER_DISPLAY_NAME = "All Content types images";
-//
-//    private static final String FOLDER_NAME = "all-content-types-images";
-//
-//    private ContentService contentService;
-//
-//    @Activate
-//    public void initialize()
-//        throws Exception
-//    {
-//        createImages();
-//    }
-//
-//    private boolean hasContent( final ContentPath path )
-//    {
-//        try
-//        {
-//            LOG.info( "path is  " + path );
-//            return this.contentService.getByPath( path ) != null;
-//        }
-//        catch ( final Exception e )
-//        {
-//            return false;
-//        }
-//    }
-//
-//    private void createImages()
-//        throws Exception
-//    {
-//        final ContentPath imageArchivePath = ContentPath.from( ContentPath.ROOT, FOLDER_NAME );
-//        if ( hasContent( imageArchivePath ) )
-//        {
-//            LOG.info( "Already initialized with data. Skipping." );
-//            return;
-//        }
-//
-//        LOG.info( "Initializing content for 'All content types selenium tests' " );
-//        final long tm = System.currentTimeMillis();
-//
-//        try
-//        {
-//            doCreateImages();
-//        }
-//        finally
-//        {
-//            LOG.info( "Initialized content for 'All content types' in " + ( System.currentTimeMillis() - tm ) + " ms" );
-//        }
-//
-//    }
-//
-//    private void doCreateImages()
-//        throws Exception
-//    {
-//
-//        final ContentPath imageArchivePath = contentService.create( createFolder() ).getPath();
-//
-//        for ( final String fileName : FOLDER_IMAGES )
-//        {
-//            createImageContent( imageArchivePath, fileName );
-//        }
-//
-//    }
-//
-//    private void createImageContent( final ContentPath parent, final String fileName )
-//        throws Exception
-//    {
-//
-//        final byte[] bytes = loadImageFileAsBytes( fileName );
-//        if ( bytes == null )
-//        {
-//            return;
-//        }
-//
-//        final CreateMediaParams params = new CreateMediaParams().
-//            mimeType( "image/jpeg" ).
-//            name( fileName ).
-//            parent( parent ).byteSource( ByteSource.wrap( bytes ) );
-//        contentService.create( params ).getId();
-//    }
-//
-//    private byte[] loadImageFileAsBytes( final String fileName )
-//    {
-//        final String filePath = "/cms/images/" + fileName;
-//
-//        try
-//        {
-//            return Resources.toByteArray( getClass().getResource( filePath ) );
-//        }
-//        catch ( Exception e )
-//        {
-//            return null;
-//        }
-//    }
-//
-//    private CreateContentParams createFolder()
-//    {
-//
-//        return CreateContentParams.create().
-//
-//            type( ContentTypeName.templateFolder() ).parent( ContentPath.ROOT ).displayName( FOLDER_DISPLAY_NAME ).
-//            requireValid( true ).owner( PrincipalKey.ofAnonymous() ).
-//            contentData( new PropertyTree() ).type( ContentTypeName.folder() ).
-//            build();
-//
-//    }
-//
-//    private <T> T runAs( final PrincipalKey role, final Callable<T> runnable )
-//    {
-//        final AuthenticationInfo authInfo = AuthenticationInfo.create().principals( role ).user( User.ANONYMOUS ).build();
-//        return ContextBuilder.from( ContextAccessor.current() ).authInfo( authInfo ).build().callWith( runnable );
-//    }
-//    @Reference
-//    public void setContentService( final ContentService contentService )
-//    {
-//        this.contentService = contentService;
-//    }
-//
-//}
