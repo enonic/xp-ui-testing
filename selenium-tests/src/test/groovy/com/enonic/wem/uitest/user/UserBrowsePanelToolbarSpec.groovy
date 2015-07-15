5f313f32ca7c8bc9841757580cf4c3d57272590a
Sergei Gavrusev <sig@enonic.com>
2015-07-15 13:33:06 +0300

    duplicate button not displayed now

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/user/UserBrowsePanelToolbarSpec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/user/UserBrowsePanelToolbarSpec.groovy
@@ -1,6 +1,7 @@
 package com.enonic.wem.uitest.user
 
 import com.enonic.autotests.pages.usermanager.browsepanel.UserBrowsePanel
+import spock.lang.Ignore
 
 class UserBrowsePanelToolbarSpec
     extends BaseUsersSpec
@@ -26,6 +27,7 @@ class UserBrowsePanelToolbarSpec
         !userBrowsePanel.isEditButtonEnabled();
     }
 
+    @Ignore
     def "GIVEN user BrowsePanel WHEN no selected content THEN 'Duplicate' button should be disabled"()
     {
         expect:
