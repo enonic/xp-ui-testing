83ae2ea6fc8b993cc17084fca28d93fffbaf5553
Sergei Gavrusev <sig@enonic.com>
2015-08-10 12:22:24 +0300

    saving of screenshot added

--- selenium-tests/src/main/java/com/enonic/autotests/pages/BrowsePanel.java
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/BrowsePanel.java
@@ -93,8 +93,9 @@ public abstract class BrowsePanel
 
     private BrowsePanel clickOnShowFilterPanelButton()
     {
-        if ( findElements( By.xpath( SHOW_FILTER_PANEL_BUTTON ) ).size() == 0 )
+        if ( findElements( By.xpath( SHOW_FILTER_PANEL_BUTTON ) ).stream().filter( WebElement::isDisplayed ).count() > 0 )
         {
+            TestUtils.saveScreenshot( getSession(), NameHelper.uniqueName( "err_show_filter" ) );
             throw new TestFrameworkException( "button 'show filter panel' not displayed or probably bad locator for web element" );
         }
         showFilterPanelButton.click();
