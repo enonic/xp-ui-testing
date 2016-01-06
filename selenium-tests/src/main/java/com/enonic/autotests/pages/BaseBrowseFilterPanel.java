34a0cced3c0d69afffd6a0ff2caa4967ed93b516
Sergei Gavrusev <sig@enonic.com>
2016-01-06 12:38:19 +0300

    timeout increased

--- selenium-tests/src/main/java/com/enonic/autotests/pages/BaseBrowseFilterPanel.java
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/BaseBrowseFilterPanel.java
@@ -68,8 +68,7 @@ public abstract class BaseBrowseFilterPanel
             throw new TestFrameworkException( "filter panel or search input not displayed" );
         }
         clearAndType( searchInput, text );
-
-        sleep( 1000 );
+        sleep( 1500 );
         getLogger().info( "Filtered by : " + text );
         return this;
     }
