43f847fc0f04eef5da2efec1722bc8a46e7f8581
sig <HauruseuKostik2012>
2016-01-18 15:51:35 +0100

    fix for one failed test

--- selenium-tests/src/main/java/com/enonic/autotests/pages/LoginPage.java
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/LoginPage.java
@@ -14,8 +14,7 @@ import com.enonic.autotests.utils.TestUtils;
 import static com.enonic.autotests.utils.SleepHelper.sleep;
 
 public class LoginPage
-    extends Page
-{
+        extends Page {
 
     private String TITLE = "Enonic XP Admin";
 
@@ -37,25 +36,22 @@ public class LoginPage
      *
      * @param session
      */
-    public LoginPage( TestSession session )
-    {
-        super( session );
+    public LoginPage(TestSession session) {
+        super(session);
     }
 
-    public HomePage doLogin( String username, String password )
-    {
-        getLogger().info( "try to login with userName:" + username + " password: " + password );
-        LoginPage loginPage = new LoginPage( getSession() );
-        loginPage.typeNameAndPassword( username, password );
-        getSession().setLoggedIn( true );
-        HomePage homePage = new HomePage( getSession() );
+    public HomePage doLogin(String username, String password) {
+        getLogger().info("try to login with userName:" + username + " password: " + password);
+        LoginPage loginPage = new LoginPage(getSession());
+        loginPage.typeNameAndPassword(username, password);
+        getSession().setLoggedIn(true);
+        LauncherPanel launcherPanel = new LauncherPanel(getSession());
         sleep(1000);
-        if ( !homePage.waitUntilLoaded() )
-        {
-            TestUtils.saveScreenshot( getSession(), NameHelper.uniqueName( "err_homepage" ) );
-            throw new AuthenticationException( "Authentication failed, home page was not opened!" );
+        if (!launcherPanel.isDisplayed()) {
+            TestUtils.saveScreenshot(getSession(), NameHelper.uniqueName("err_login"));
+            throw new AuthenticationException("Authentication failed, launcher panel was not loaded!");
         }
-        return homePage;
+        return new HomePage(getSession());
     }
 
     /**
@@ -64,26 +60,23 @@ public class LoginPage
      * @param username
      * @param password
      */
-    public void typeNameAndPassword( String username, String password )
-    {
-        boolean isLoginPageLoaded = waitUntilTitleLoad( TITLE, LOGIN_PAGE_TIMEOUT );
-        if ( !isLoginPageLoaded )
-        {
-            String name = NameHelper.uniqueName( "login_page_error" );
-            TestUtils.saveScreenshot( getSession(), name );
-            throw new TestFrameworkException( "Login page was not loaded, timeout sec:" + LOGIN_PAGE_TIMEOUT );
+    public void typeNameAndPassword(String username, String password) {
+        boolean isLoginPageLoaded = waitUntilTitleLoad(TITLE, LOGIN_PAGE_TIMEOUT);
+        if (!isLoginPageLoaded) {
+            String name = NameHelper.uniqueName("login_page_error");
+            TestUtils.saveScreenshot(getSession(), name);
+            throw new TestFrameworkException("Login page was not loaded, timeout sec:" + LOGIN_PAGE_TIMEOUT);
         }
-        getLogger().info( "Login action started. Username: " + username + " Password:" + password );
+        getLogger().info("Login action started. Username: " + username + " Password:" + password);
 
-        boolean isEmailInputPresent = waitAndFind( By.xpath( EMAIL_INPUT_XPATH ) );
-        if ( !isEmailInputPresent )
-        {
-            throw new TestFrameworkException( " input 'user id or e-mail' was not found on page!" );
+        boolean isEmailInputPresent = waitAndFind(By.xpath(EMAIL_INPUT_XPATH));
+        if (!isEmailInputPresent) {
+            throw new TestFrameworkException(" input 'user id or e-mail' was not found on page!");
         }
-        clearAndType( usernameInput, username );
-        sleep( 300 );
-        clearAndType( passwordInput, password );
-        sleep( 300 );
-        passwordInput.sendKeys( Keys.ENTER );
+        clearAndType(usernameInput, username);
+        sleep(300);
+        clearAndType(passwordInput, password);
+        sleep(300);
+        passwordInput.sendKeys(Keys.ENTER);
     }
 }
