bbfc42656ec75ab75de90319a12004b774631269
Sergei Gavrusev <sig@enonic.com>
2016-03-02 18:08:50 +0300

    fix for one failed test

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/application/BaseApplicationSpec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/application/BaseApplicationSpec.groovy
@@ -70,6 +70,7 @@ class BaseApplicationSpec
             marketPanel.doInstallApp( displayName );
             appDialog.clickOnCancelButton();
             sleep( 3000 );
+            applicationBrowsePanel.waitApplicationStatus( name, "started" )
         }
     }
 }
