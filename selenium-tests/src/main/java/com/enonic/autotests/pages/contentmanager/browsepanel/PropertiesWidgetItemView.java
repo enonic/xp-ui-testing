1dea7f31ab8934d889e4e668da5bc5b65ab510e5
Sergei Gavrusev <sig@enonic.com>
2016-03-05 18:16:01 +0300

    XP-2614 Create selenium tests for 'PropertiesWidgetItemView' (Details Panel)

---
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/contentmanager/browsepanel/PropertiesWidgetItemView.java
@@ -0,0 +1,109 @@
+package com.enonic.autotests.pages.contentmanager.browsepanel;
+
+import com.enonic.autotests.TestSession;
+import com.enonic.autotests.pages.Application;
+
+
+public class PropertiesWidgetItemView
+    extends Application
+
+{
+    private final String DIV_CONTAINER = "//div[contains(@id,'PropertiesWidgetItemView')]";
+
+    private final String TYPE_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Type:')]/following-sibling::dt[1]";
+
+    private final String OWNER_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Owner:')]/following-sibling::dt[1]";
+
+    private final String APPLICATION_NAME_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Application:')]/following-sibling::dt[1]";
+
+    private final String ID_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Id:')]/following-sibling::dt[1]";
+
+    private final String MODIFIED_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Modified:')]/following-sibling::dt[1]";
+
+    private final String CREATED_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Created:')]/following-sibling::dt[1]";
+
+    private final String LANGUAGE_PROPERTY = DIV_CONTAINER + "//dd[contains(.,'Language:')]/following-sibling::dt[1]";
+
+    public PropertiesWidgetItemView( final TestSession session )
+    {
+        super( session );
+    }
+
+    public boolean isDisplayed()
+    {
+        return isElementDisplayed( DIV_CONTAINER );
+    }
+
+    public boolean isTypePresent()
+    {
+        return isElementDisplayed( TYPE_PROPERTY );
+    }
+
+    public boolean isLanguagePresent()
+    {
+        return isElementDisplayed( LANGUAGE_PROPERTY );
+    }
+
+    public boolean isIdPresent()
+    {
+        return isElementDisplayed( ID_PROPERTY );
+    }
+
+    public boolean isCreatedPresent()
+    {
+        return isElementDisplayed( CREATED_PROPERTY );
+    }
+
+    public boolean isIdModifiedPresent()
+    {
+        return isElementDisplayed( MODIFIED_PROPERTY );
+    }
+
+    public boolean isApplicationNamePresent()
+    {
+        return isElementDisplayed( APPLICATION_NAME_PROPERTY );
+    }
+
+    public boolean isOwnerPresent()
+    {
+        return isElementDisplayed( OWNER_PROPERTY );
+    }
+
+    public String getType()
+    {
+        return getDisplayedString( TYPE_PROPERTY );
+    }
+
+    public String getOwner()
+    {
+        return getDisplayedString( OWNER_PROPERTY );
+    }
+
+    public String getModified()
+    {
+        return getDisplayedString( MODIFIED_PROPERTY );
+    }
+
+    public String getCreated()
+    {
+        return getDisplayedString( CREATED_PROPERTY );
+    }
+
+    public String getId()
+    {
+        return getDisplayedString( ID_PROPERTY );
+    }
+
+    public String getApplicationName()
+    {
+        return getDisplayedString( APPLICATION_NAME_PROPERTY );
+    }
+
+    public String getLanguage()
+    {
+        return getDisplayedString( LANGUAGE_PROPERTY );
+    }
+
+
+}
+
