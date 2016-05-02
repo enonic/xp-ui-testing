553787c187066601873d0a8afb744a471ef018c2
Sergei Gavrusev <sig@enonic.com>
2016-05-02 14:23:46 +0300

    fix for one failed test

--- selenium-tests/src/main/java/com/enonic/autotests/pages/contentmanager/browsepanel/ContentDetailsPanel.java
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/contentmanager/browsepanel/ContentDetailsPanel.java
@@ -2,6 +2,7 @@ package com.enonic.autotests.pages.contentmanager.browsepanel;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 import com.enonic.autotests.TestSession;
@@ -15,6 +16,8 @@ import static com.enonic.autotests.utils.SleepHelper.sleep;
 public class ContentDetailsPanel
     extends Application
 {
+    private final String SPLIT_PANEL_WITH_DETAILS_PANEL = "//div[contains(@class,'split-panel-with-details')]";
+
     public static final String DETAILS_PANEL = "//div[contains(@id,'ContentBrowsePanel')]//div[contains(@id,'DetailsPanel')]";
 
     private final String VERSION_HISTORY_OPTION = "//div[text()='Version history']";
@@ -95,9 +98,15 @@ public class ContentDetailsPanel
         return isElementDisplayed( DETAILS_PANEL + String.format( NAMES_VIEW_BY_DISPLAY_NAME, contentDisplayName ) );
     }
 
+    /**
+     * @return true if details panel, that located on 'split panel' is displayed
+     */
     public boolean isDisplayed()
     {
-        return isElementDisplayed( DETAILS_PANEL );
+        WebElement splitPanel = getDisplayedElement( By.xpath( SPLIT_PANEL_WITH_DETAILS_PANEL ) );
+        JavascriptExecutor executor = (JavascriptExecutor) getSession().getDriver();
+        return !(Boolean) executor.executeScript(
+            "return window.api.dom.ElementRegistry.getElementById(arguments[0]).isSecondPanelHidden()", splitPanel.getAttribute( "id" ) );
     }
 
     public String getContentDisplayName()
