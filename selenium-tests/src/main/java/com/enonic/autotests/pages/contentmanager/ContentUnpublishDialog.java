6735413eef08a603f5c9f283059d4aaa79e4d765
Sergei Gavrusev <sig@enonic.com>
2016-05-23 11:20:57 +0300

    missed class added

---
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/contentmanager/ContentUnpublishDialog.java
@@ -0,0 +1,80 @@
+package com.enonic.autotests.pages.contentmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.enonic.autotests.TestSession;
+import com.enonic.autotests.exceptions.TestFrameworkException;
+import com.enonic.autotests.pages.Application;
+import com.enonic.autotests.utils.TestUtils;
+
+import static com.enonic.autotests.utils.SleepHelper.sleep;
+
+public class ContentUnpublishDialog
+    extends Application
+{
+    private final String DIALOG_CONTAINER = "//div[contains(@id,'ContentUnpublishDialog')]";
+
+    private final String UNPUBLISH_BUTTON =
+        DIALOG_CONTAINER + "//button[contains(@id,'DialogButton') and descendant::span[contains(.,'Unpublish')]]";
+
+    private final String CANCEL_BUTTON_TOP = DIALOG_CONTAINER + "//div[contains(@class,'cancel-button-top')]";
+
+    private final String CANCEL_BUTTON_BOTTOM = DIALOG_CONTAINER + "//button[contains(@class,'cancel-button-bottom')]";
+
+    @FindBy(xpath = UNPUBLISH_BUTTON)
+    private WebElement unPublishButton;
+
+    @FindBy(xpath = CANCEL_BUTTON_TOP)
+    private WebElement cancelButtonTop;
+
+    @FindBy(xpath = CANCEL_BUTTON_BOTTOM)
+    private WebElement cancelButtonBottom;
+
+
+    public ContentUnpublishDialog( TestSession session )
+    {
+        super( session );
+    }
+
+    public boolean isOpened()
+    {
+        return isElementDisplayed( DIALOG_CONTAINER );
+    }
+
+    public ContentUnpublishDialog waitUntilDialogShown( long timeout )
+    {
+        if ( !waitUntilVisibleNoException( By.xpath( DIALOG_CONTAINER ), timeout ) )
+        {
+            TestUtils.saveScreenshot( getSession(), "err_unpublish_dialog_not_opened" );
+            throw new TestFrameworkException( "Content unpublish dialog was not shown!" );
+        }
+        return this;
+    }
+
+    public boolean isUnPublishNowButtonEnabled()
+    {
+        return unPublishButton.isEnabled();
+    }
+
+    public void clickOnUnpublishButton()
+    {
+        unPublishButton.click();
+        sleep( 200 );
+    }
+
+    public void clickOnCancelBottomButton()
+    {
+        cancelButtonBottom.click();
+        sleep( 200 );
+    }
+
+    public void clickOnCanceltopButton()
+    {
+        cancelButtonTop.click();
+        sleep( 200 );
+    }
+
+
+}
