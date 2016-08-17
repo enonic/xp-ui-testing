8e767cee5e9896ad3ec00e7bfaec0672c206528a
Sergei Gavrusev <sig@enonic.com>
2016-08-17 16:07:33 +0300

    XP-3946 Add selenium test for verifying of "XP-3885 Dates in December breaks the browser"

---
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/input_types/DateTime_April_Spec.groovy
@@ -0,0 +1,82 @@
+package com.enonic.wem.uitest.content.input_types
+
+import com.enonic.autotests.pages.contentmanager.wizardpanel.ContentWizardPanel
+import com.enonic.autotests.pages.form.DateTimeFormViewPanel
+import com.enonic.autotests.vo.contentmanager.Content
+import spock.lang.Shared
+
+/**
+ * Created  on 17.08.2016.
+ *  verifies   the https://youtrack.enonic.net/issue/XP-3885
+ * */
+class DateTime_April_Spec
+    extends Base_InputFields_Occurrences
+{
+
+    @Shared
+    String DATE_TIME_1 = "2016-03-01 00:00";
+
+    @Shared
+    String DATE_TIME_2 = "2016-04-01 00:00"
+
+    @Shared
+    String DATE_TIME_3 = "2016-03-31 01:01"
+
+    def "GIVEN wizard for adding a DateTime with tz opened WHEN datetime in March typed AND saved THEN correct value displayed in the wizard"()
+    {
+        given: "wizard for adding a DateTime with timezone opened"
+        Content dateTimeContent = buildDateTime1_1_Content( DATE_TIME_1 );
+        ContentWizardPanel contentWizardPanel = selectSiteOpenWizard( dateTimeContent.getContentTypeName() );
+
+
+        when: "DateTime in March typed and content saved"
+        contentWizardPanel.typeData( dateTimeContent ).save().close( dateTimeContent.getDisplayName() );
+
+        and: "just created datetime content opened"
+        contentBrowsePanel.clickOnClearSelection();
+        findAndSelectContent( dateTimeContent.getName() ).clickToolbarEdit().waitUntilWizardOpened();
+        DateTimeFormViewPanel formViewPanel = new DateTimeFormViewPanel( getSession() );
+
+        then: "actual value and expected are equals"
+        formViewPanel.getDateTimeValue() == DATE_TIME_1;
+    }
+
+    def "GIVEN wizard for adding a DateTime with tz opened WHEN second datetime in March typed AND saved THEN correct value displayed in the wizard"()
+    {
+        given: "wizard for adding a DateTime with timezone opened"
+        Content dateTimeContent = buildDateTime1_1_Content( DATE_TIME_2 );
+        ContentWizardPanel contentWizardPanel = selectSiteOpenWizard( dateTimeContent.getContentTypeName() );
+
+
+        when: "DateTime in April typed and content saved"
+        contentWizardPanel.typeData( dateTimeContent ).save().close( dateTimeContent.getDisplayName() );
+
+        and: "just created datetime content opened"
+        contentBrowsePanel.clickOnClearSelection();
+        findAndSelectContent( dateTimeContent.getName() ).clickToolbarEdit().waitUntilWizardOpened();
+        DateTimeFormViewPanel formViewPanel = new DateTimeFormViewPanel( getSession() );
+
+        then: "actual value and expected are equals"
+        formViewPanel.getDateTimeValue() == DATE_TIME_2;
+    }
+
+    def "GIVEN wizard for adding a DateTime with tz opened WHEN third datetime in March typed AND saved THEN correct value displayed in the wizard"()
+    {
+        given: "wizard for adding a DateTime with timezone opened"
+        Content dateTimeContent = buildDateTime1_1_Content( DATE_TIME_3 );
+        ContentWizardPanel contentWizardPanel = selectSiteOpenWizard( dateTimeContent.getContentTypeName() );
+
+
+        when: "DateTime in March typed and content saved"
+        contentWizardPanel.typeData( dateTimeContent ).save().close( dateTimeContent.getDisplayName() );
+
+        and: "just created datetime content opened"
+        contentBrowsePanel.clickOnClearSelection();
+        findAndSelectContent( dateTimeContent.getName() ).clickToolbarEdit().waitUntilWizardOpened();
+        DateTimeFormViewPanel formViewPanel = new DateTimeFormViewPanel( getSession() );
+
+        then: "actual value and expected are equals"
+        formViewPanel.getDateTimeValue() == DATE_TIME_3;
+    }
+
+}
---
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/input_types/DatesInDecember_Spec.groovy
@@ -0,0 +1,78 @@
+package com.enonic.wem.uitest.content.input_types
+
+import com.enonic.autotests.pages.contentmanager.wizardpanel.ContentWizardPanel
+import com.enonic.autotests.pages.form.DateFormViewPanel
+import com.enonic.autotests.vo.contentmanager.Content
+import spock.lang.Shared
+
+/**
+ * Created on 16.08.2016.
+ *
+ *  verifies   the https://youtrack.enonic.net/issue/XP-3885
+ * */
+class DatesInDecember_Spec
+    extends Base_InputFields_Occurrences
+{
+    @Shared
+    String DATE_IN_DECEMBER1 = "2016-12-31";
+
+    @Shared
+    String DATE_IN_DECEMBER2 = "1999-12-31";
+
+    @Shared
+    String DATE_IN_DECEMBER3 = "2099-12-31";
+
+    def "GIVEN wizard for adding a Date opened WHEN date in December typed AND saved THEN correct value displayed in the wizard"()
+    {
+        given: "wizard for adding a Date opened"
+        Content dateContent = buildDate1_1_Content( DATE_IN_DECEMBER1 );
+        ContentWizardPanel contentWizardPanel = selectSiteOpenWizard( dateContent.getContentTypeName() );
+
+        when: "Date in December typed and content saved"
+        contentWizardPanel.typeData( dateContent ).save().close( dateContent.getDisplayName() );
+
+        and: "just created date content opened"
+        contentBrowsePanel.clickOnClearSelection();
+        findAndSelectContent( dateContent.getName() ).clickToolbarEdit().waitUntilWizardOpened();
+        DateFormViewPanel dateFormViewPanel = new DateFormViewPanel( getSession() );
+
+        then: "actual value and expected are equals"
+        dateFormViewPanel.getDateValue() == DATE_IN_DECEMBER1;
+    }
+
+    def "GIVEN wizard for adding a Date opened WHEN second date in December typed AND saved THEN correct value displayed in the wizard"()
+    {
+        given: "wizard for adding a Date opened"
+        Content dateContent = buildDate1_1_Content( DATE_IN_DECEMBER2 );
+        ContentWizardPanel contentWizardPanel = selectSiteOpenWizard( dateContent.getContentTypeName() );
+
+        when: "Date in December typed and content saved"
+        contentWizardPanel.typeData( dateContent ).save().close( dateContent.getDisplayName() );
+
+        and: "just created date content opened"
+        contentBrowsePanel.clickOnClearSelection();
+        findAndSelectContent( dateContent.getName() ).clickToolbarEdit().waitUntilWizardOpened();
+        DateFormViewPanel dateFormViewPanel = new DateFormViewPanel( getSession() );
+
+        then: "actual value and expected are equals"
+        dateFormViewPanel.getDateValue() == DATE_IN_DECEMBER2;
+    }
+
+    def "GIVEN wizard for adding a Date opened WHEN thisrd date in December typed AND saved THEN correct value displayed in the wizard"()
+    {
+        given: "wizard for adding a Date opened"
+        Content dateContent = buildDate1_1_Content( DATE_IN_DECEMBER3 );
+        ContentWizardPanel contentWizardPanel = selectSiteOpenWizard( dateContent.getContentTypeName() );
+
+        when: "Date in December typed and content saved"
+        contentWizardPanel.typeData( dateContent ).save().close( dateContent.getDisplayName() );
+
+        and: "just created date content opened"
+        contentBrowsePanel.clickOnClearSelection();
+        findAndSelectContent( dateContent.getName() ).clickToolbarEdit().waitUntilWizardOpened();
+        DateFormViewPanel dateFormViewPanel = new DateFormViewPanel( getSession() );
+
+        then: "actual value and expected are equals"
+        dateFormViewPanel.getDateValue() == DATE_IN_DECEMBER3;
+    }
+}
