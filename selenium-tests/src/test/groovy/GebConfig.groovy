6dce24abf41d3e4b9a6492530f37c92a0ad8057f
Sergei Gavrusev <sig@enonic.com>
2016-11-16 13:39:44 +0300

    XP-4481 Update configuration for tests on FF with geckodriver

--- selenium-tests/src/test/groovy/GebConfig.groovy
+++ selenium-tests/src/test/groovy/GebConfig.groovy
@@ -1,8 +1,7 @@
 import org.openqa.selenium.Platform
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxProfile
+import org.openqa.selenium.firefox.MarionetteDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 
@@ -43,8 +42,6 @@ driver = {
     }
 
     def driver = new ChromeDriver();
-    System.setProperty( "webdriver.gecko.driver", "c:/gecko/geckodriver.exe" );
-    System.setProperty( "webdriver.firefox.marionette", "c:/gecko/geckodriver.exe" );
     println "screen height is " + driver.manage().window().getSize().height;
     println "screen width is " + driver.manage().window().getSize().width;
     println "default configuration";
@@ -90,11 +87,31 @@ environments {
     }
     firefox {
         driver = {
-            FirefoxProfile profile = new FirefoxProfile();
-            profile.setEnableNativeEvents( true );
-            def driver = new FirefoxDriver( profile );
-            //System.setProperty("webdriver.firefox.marionette","C:/gecko/geckodriver.exe");
-            //System.setProperty("webdriver.gecko.driver","C:/gecko/geckodriver.exe");
+            Properties props = new Properties()
+            File propsFile = new File( 'tests.properties' )
+            props.load( propsFile.newDataInputStream() )
+            def pathToDriver;
+            if ( Platform.current.is( Platform.WINDOWS ) )
+            {
+                pathToDriver = props.getProperty( 'windows.gecko.path' )
+            }
+
+            else if ( Platform.current.is( Platform.LINUX ) )
+            {
+                pathToDriver = props.getProperty( 'linux.gecko.path' )
+            }
+            else if ( Platform.current.is( Platform.MAC ) )
+            {
+                pathToDriver = props.getProperty( 'mac.gecko.path' )
+            }
+            else
+            {
+                throw new RuntimeException( "Unsupported operating system [${Platform.current}]" )
+            }
+            System.setProperty( "webdriver.gecko.driver", pathToDriver );
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            capabilities.setCapability( "marionette", true );
+            def driver = new MarionetteDriver( capabilities );
             driver.setLogLevel( Level.INFO )
             driver.manage().window().maximize()
             println "firefox configuration"
