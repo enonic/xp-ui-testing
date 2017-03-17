43457ecc9e56adeb8245494490c2078ae231b8f3
sgauruseu <sig@enonic.com>
2017-03-17 11:29:08 +0300

    xp-ui-testing#25 Update Selenium tests in order to new changes in the branch 'issue-4567'

---
+++ selenium-tests/src/main/java/com/enonic/autotests/pages/contentmanager/ContentMenuItem.java
@@ -0,0 +1,23 @@
+package com.enonic.autotests.pages.contentmanager;
+
+/**
+ * Created  on 3/16/2017.
+ */
+public enum ContentMenuItem
+{
+
+    NEW( "New..." ), EDIT( "Edit" ), DELETE( "Delete..." ), DUPLICATE( "Duplicate" ), MOVE( "Move..." ), SORT( "Sort..." ), PREVIEW(
+    "Preview" ), PUBLISH( "Publish..." ), UNPUBLISH( "Unpublish..." );
+
+    private String name;
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    private ContentMenuItem( String name )
+    {
+        this.name = name;
+    }
+}
