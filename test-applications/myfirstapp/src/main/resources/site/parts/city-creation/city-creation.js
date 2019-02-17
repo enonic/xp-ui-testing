8a10a9093d23e6aacb9caedbb136036338418b68
Siarhei Hauruseu <sig@enonic.com>
2019-02-17 14:25:42 +0300

    test-apps updated

--- test-applications/myfirstapp/src/main/resources/site/parts/city-creation/city-creation.js
+++ test-applications/myfirstapp/src/main/resources/site/parts/city-creation/city-creation.js
@@ -3,7 +3,7 @@ var thymeleaf = require('/lib/thymeleaf');
 var contentSvc = require('/lib/xp/content');
 
 var view = resolve('city-creation.page.html');
-var service = require('service.js').service;
+var service = require('/lib/service.js').service;
 
 function handleGet(req) {
     var cityServiceUrl = service.serviceUrl('city');
