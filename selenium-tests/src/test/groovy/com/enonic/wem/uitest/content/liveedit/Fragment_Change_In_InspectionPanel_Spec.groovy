5b5cd2881494318fbc28181f1fcae6ea4a32c13b
Siarhei Hauruseu <sig@enonic.com>
2019-11-13 13:01:15 +0300

    minor changes

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/liveedit/Fragment_Change_In_InspectionPanel_Spec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/liveedit/Fragment_Change_In_InspectionPanel_Spec.groovy
@@ -54,7 +54,7 @@ class Fragment_Change_In_InspectionPanel_Spec
         siteWizard.showComponentView();
         pageComponentsView.openMenu( PART_FRAGMENT_CITY_LIST ).selectMenuItem( "Save as Fragment" );
         saveScreenshot( "fragment_inspection_panel1" );
-        sleep( 1200 );
+        sleep( 2000 );
 
         when: "'Dropdown handle' has been clicked"
         FragmentInspectionPanel fragmentInspectionPanel = new FragmentInspectionPanel( getSession() );
@@ -67,7 +67,7 @@ class Fragment_Change_In_InspectionPanel_Spec
         options.get( 0 ) == PART_FRAGMENT_CITY_LIST
     }
 
-    def "GIVEN existing site with one fragment is opened WHEN one more fragment has been created THEN two fragments should be present in the dropdown list"()
+    def "GIVEN existing site with single fragment is opened WHEN one more fragment has been created THEN two fragments should be present in the dropdown list"()
     {
         given:
         ContentWizardPanel siteWizard = findAndSelectContent( SITE1.getName() ).clickToolbarEdit();
