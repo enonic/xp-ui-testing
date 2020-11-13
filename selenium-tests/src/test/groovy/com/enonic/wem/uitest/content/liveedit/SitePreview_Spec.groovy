4e9b2568fda87fd5a15c43c24f41a0e13b70c9a4
Siarhei Hauruseu <sig@enonic.com>
2020-11-13 11:47:46 +0300

    test's description updated

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/liveedit/SitePreview_Spec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/liveedit/SitePreview_Spec.groovy
@@ -14,21 +14,21 @@ class SitePreview_Spec
     Content MY_SITE
 
 
-    def "GIVEN existing site without a template WHEN site has been selected THEN 'Preview' on a BrowseToolbar should be disabled"()
+    def "GIVEN existing site is selected WHEN controller is not selected THEN 'Preview' button should be disabled"()
     {
         given:
         MY_SITE = buildMyFirstAppSite( "preview" );
         "data typed and saved and wizard closed"
         addSite( MY_SITE );
 
-        when: "site without a template selected"
+        when: "site without a controller has been selected"
         findAndSelectContent( MY_SITE.getName() );
 
         then: "'Preview' on a BrowseToolbar is disabled"
         !contentBrowsePanel.isPreviewButtonEnabled();
     }
 
-    def "GIVEN existing site without a template WHEN site is selected THEN 'Preview' in the ContextMenu should be disabled"()
+    def "GIVEN existing site is selected WHEN controller is not selected THEN 'Preview' in the ContextMenu should be disabled"()
     {
         when: "site is selected"
         filterPanel.typeSearchText( MY_SITE.getName() );
@@ -38,7 +38,7 @@ class SitePreview_Spec
         contentBrowsePanel.waitUntilItemDisabledInContextMenu( MY_SITE.getName(), "Preview" );
     }
 
-    def "GIVEN existing site without a template WHEN site is opened THEN 'Preview' button should be disabled in the wizard"()
+    def "GIVEN existing site is opened WHEN controller is not selected THEN 'Preview' button should be disabled"()
     {
         when: "existing has been opened"
         ContentWizardPanel wizard = findAndSelectContent( MY_SITE.getName() ).clickToolbarEdit();
@@ -47,7 +47,7 @@ class SitePreview_Spec
         !wizard.isPreviewButtonEnabled()
     }
 
-    def "GIVEN existing site without a template WHEN the site is opened AND the controller has been selectedselected THEN 'Preview' on a BrowseToolbar becomes enabled"()
+    def "WHEN existing site is opened AND a controller has been selected THEN 'Preview' in BrowseToolbar gets enabled"()
     {
         given: "existing site without a template is opened"
         ContentWizardPanel wizard = findAndSelectContent( MY_SITE.getName() ).clickToolbarEdit();
@@ -56,11 +56,12 @@ class SitePreview_Spec
         wizard.selectPageDescriptor( COUNTRY_REGION_TITLE ).closeBrowserTab().switchToBrowsePanelTab();
         saveScreenshot( "site-template-preview" );
 
-        then: "'Preview' on the BrowseToolbar should be enabled"
+        then: "'Preview' in the BrowseToolbar should be enabled"
+        findAndSelectContent( MY_SITE.getName() );
         contentBrowsePanel.isPreviewButtonEnabled();
     }
 
-    def "WHEN existing site with the page-template was selected AND context menu is shown THEN 'Preview' menu item should be enabled in the ContextMenu"()
+    def "WHEN existing site is selected AND context menu has been opened THEN 'Preview' menu item should be enabled"()
     {
         when: "existing site with the page-template was selected AND context menu is shown"
         filterPanel.typeSearchText( MY_SITE.getName() );
@@ -69,7 +70,7 @@ class SitePreview_Spec
         contentBrowsePanel.openContextMenuAndWaitUntilItemEnabled( MY_SITE.getName(), "Preview" );
     }
 
-    def "WHEN existing site with the page-template was selected  THEN 'Preview' button should be enabled on the toolbar wizard"()
+    def "WHEN existing site with controller has been opened THEN 'Preview' button should be enabled"()
     {
         when: "site is selected"
         filterPanel.typeSearchText( MY_SITE.getName() );
@@ -79,17 +80,17 @@ class SitePreview_Spec
         wizard.isPreviewButtonEnabled()
     }
     // test for verifying of XP-4123 (Page Editor inaccessible for a folder)
-    def "GIVEN existing site with a selected controller WHEN child-folder has been added to the site THEN 'Show Page Editor' button should be present on the wizard toolbar"()
+    def "WHEN site's child-folder has been opened THEN 'Show Page Editor' button should be present in the wizard toolbar"()
     {
-        given: "existing site with the page-template was selected and 'New' button pressed"
+        given: "existing site with selected page-template has been selected and 'New' button pressed"
         ContentWizardPanel wizard = findAndSelectContent( MY_SITE.getName() ).clickToolbarNew().selectContentType(
             BaseContentType.FOLDER.getDisplayName() );
 
-        when: "child-folder has been added for the site"
+        when: "child-folder has been added"
         wizard.typeDisplayName( "test-page-editor-toggler" ).save();
         saveScreenshot( "folder-show-page-editor-toggler" );
 
-        then: "'Show Page Editor' button should be present on the wizard-toolbar"
+        then: "'Show Page Editor' button should be present in the wizard-toolbar"
         wizard.isShowPageEditorButtonDisplayed();
     }
 }
