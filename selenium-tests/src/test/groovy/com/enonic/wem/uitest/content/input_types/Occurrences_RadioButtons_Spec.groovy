439eec24b69101b7da767bcbaf6359fbc500f37b
Siarhei Hauruseu <sig@enonic.com>
2021-06-25 13:55:20 +0300

    minor changes

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/input_types/Occurrences_RadioButtons_Spec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/input_types/Occurrences_RadioButtons_Spec.groovy
@@ -7,13 +7,47 @@ import com.enonic.autotests.pages.form.SingleSelectorRadioFormView
 import com.enonic.autotests.utils.NameHelper
 import com.enonic.autotests.vo.contentmanager.Content
 import spock.lang.Shared
+import spock.lang.Stepwise
 
+@Stepwise
 class Occurrences_RadioButtons_Spec
     extends Base_InputFields_Occurrences
 
 {
     @Shared
     Content content_wit_opt;
+
+    def "GIVEN new 'Single Selector Radio-content' is created (option was selected) WHEN the content is reopened THEN expected option should be selected"()
+    {
+        given: "new content with type 'Single Selector Radio' is saved"
+        String option = "option A";
+        content_wit_opt = buildRadioButtonsContent( option );
+        selectSitePressNew( content_wit_opt.getContentTypeName() ).typeData(
+            content_wit_opt ).save().closeBrowserTab().switchToBrowsePanelTab();
+
+        when: "the content has been reopened"
+        contentBrowsePanel.selectAndOpenContentFromToolbarMenu( content_wit_opt );
+        SingleSelectorRadioFormView formViewPanel = new SingleSelectorRadioFormView( getSession() );
+
+        then:"expected option should be selected"
+        formViewPanel.getSelectedOption() == option;
+    }
+
+    def "GIVEN a not required 'Single Selector Radio-content' with selected option WHEN content opened and option changed THEN new option should be selected"()
+    {
+        given: "content with type 'Single Selector Radio' is opened"
+        String newOption = "option B";
+        ContentWizardPanel contentWizardPanel = contentBrowsePanel.selectAndOpenContentFromToolbarMenu( content_wit_opt );
+
+        when: "new option has been selected"
+        SingleSelectorRadioFormView formViewPanel = new SingleSelectorRadioFormView( getSession() );
+        formViewPanel.selectOption( newOption );
+        contentWizardPanel.save().closeBrowserTab().switchToBrowsePanelTab();
+        contentBrowsePanel.selectAndOpenContentFromToolbarMenu( content_wit_opt );
+
+        then: "new selected option should be displayed"
+        formViewPanel.getSelectedOption() == newOption;
+    }
 
     def "WHEN wizard for 'Radio Buttons'-content is opened THEN radio buttons should be present in the page AND options should not be selected"()
     {
@@ -63,37 +97,5 @@ class Occurrences_RadioButtons_Spec
         !contentBrowsePanel.isContentInvalid( radioButtonsContent.getName() );
         and: "expected notification message should appear"
         publishedMessage == String.format( Application.ITEM_IS_PUBLISHED_NOTIFICATION_MESSAGE, radioButtonsContent.getName() );
-    }
-
-    def "GIVEN new 'Single Selector Radio-content' is created (option was selected) WHEN the content is reopened THEN expected option should be selected"()
-    {
-        given: "new content with type 'Single Selector Radio' is saved"
-        String option = "option A";
-        content_wit_opt = buildRadioButtonsContent( option );
-        selectSitePressNew( content_wit_opt.getContentTypeName() ).typeData(
-            content_wit_opt ).save().closeBrowserTab().switchToBrowsePanelTab();
-
-        when: "the content has been reopened"
-        contentBrowsePanel.selectAndOpenContentFromToolbarMenu( content_wit_opt );
-        SingleSelectorRadioFormView formViewPanel = new SingleSelectorRadioFormView( getSession() );
-
-        then:"expected option should be selected"
-        formViewPanel.getSelectedOption() == option;
-    }
-
-    def "GIVEN a not required 'Single Selector Radio-content' with selected option WHEN content opened and option changed THEN new option should be selected"()
-    {
-        given: "content with type 'Single Selector Radio' is opened"
-        String newOption = "option B";
-        ContentWizardPanel contentWizardPanel = contentBrowsePanel.selectAndOpenContentFromToolbarMenu( content_wit_opt );
-
-        when: "new option has been selected"
-        SingleSelectorRadioFormView formViewPanel = new SingleSelectorRadioFormView( getSession() );
-        formViewPanel.selectOption( newOption );
-        contentWizardPanel.save().closeBrowserTab().switchToBrowsePanelTab();
-        contentBrowsePanel.selectAndOpenContentFromToolbarMenu( content_wit_opt );
-
-        then: "new selected option should be displayed"
-        formViewPanel.getSelectedOption() == newOption;
     }
 }
