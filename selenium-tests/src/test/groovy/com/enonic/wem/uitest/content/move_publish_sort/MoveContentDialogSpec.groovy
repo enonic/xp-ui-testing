c173239e4e8bad14a52275b933aabdd9d170f821
Siarhei Hauruseu <sig@enonic.com>
2022-01-25 20:00:47 +0300

    skip migrated tests

--- selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/move_publish_sort/MoveContentDialogSpec.groovy
+++ selenium-tests/src/test/groovy/com/enonic/wem/uitest/content/move_publish_sort/MoveContentDialogSpec.groovy
@@ -3,10 +3,12 @@ package com.enonic.wem.uitest.content.move_publish_sort
 import com.enonic.autotests.pages.contentmanager.browsepanel.MoveContentDialog
 import com.enonic.autotests.vo.contentmanager.Content
 import com.enonic.wem.uitest.content.BaseContentSpec
+import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Stepwise
 
 @Stepwise
+@Ignore
 class MoveContentDialogSpec
     extends BaseContentSpec
 {
